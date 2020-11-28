9f295a4c0532c5752c0ba1e5b0de9829bdf40983
Connor Rose <connorrose.git@gmail.com>
2020-11-28 15:37:16 -0800

    custom useFetch hook

--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import CharacterList from './CharacterList';
 
-import dummyData from './dummy-data';
+// import dummyData from './dummy-data';
+import endpoint from './endpoint';
 
 import './styles.scss';
 
+const useFetch = (url) => {
+  const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    setResponse(null);
+
+    fetch(url)
+      .then(res => res.json())
+      .then(res => setResponse(res))
+      .catch(err => setError(err))
+      .finally(() => setLoading(false))
+  }, [url])
+
+  return [response, loading, error];
+}
+
 const Application = () => {
-  const [characters, setCharacters] = useState(dummyData);
+  const [response, loading, error] = useFetch(endpoint + '/characters');
 
   return (
     <div className="Application">
@@ -19,7 +40,9 @@ const Application = () => {
       </header>
       <main>
         <section className="sidebar">
-          <CharacterList characters={characters} />
+          {loading && <p>Loading...</p>}
+          {response && <CharacterList characters={response.characters} />}
+          {error && <p className="error">{JSON.stringify(error)}</p>}
         </section>
       </main>
     </div>
